3db194549f9a50746118a4f9f5586cb114562f8b
Vahid Mazde <vahid.mazdeh@fourwindsinteractive.com>
2016-09-20 09:21:18 -0600

    DynamoDB set up -- data gets stored on save

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var auth = require('./src/constants/auth');
+var aws = require('aws-sdk');
 
 var app = express();
+aws.config.loadFromPath('./aws_config.json');
+var db = new aws.DynamoDB({ region: 'us-west-2'});
 
 var PORT = process.env.PORT || 3000;
+
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(express.static('./dist'));
@@ -19,6 +23,22 @@ app.get('/save', function(req, res) {
 
 app.post('/alscore', function(req, res) {
   var text = req.body.text;
+
+  var dbData = {
+    TableName: 'thoughts',
+    Item: {
+      id: {'S': 'vahid'},
+      text: {'S': text}
+    }
+  }
+  db.putItem(dbData, function(err, data) {
+    if (err) {
+      console.log("couldn't save data to db.")
+      console.log('err: ', err);
+    } else {
+      console.log("form data saved");
+    }
+  })
 
   alchemy.emotions(text, {}, (err, response) => {
     if (err) throw err;
