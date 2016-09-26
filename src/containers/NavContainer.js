a2a9fa2c7ca1b6fda851310606f0dbd9948d72b5
Vahid Mazde <vahid.mazdeh@fourwindsinteractive.com>
2016-09-26 11:13:35 -0600

    login UI to get things started

--- src/containers/NavContainer.js
+++ src/containers/NavContainer.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Nav extends Component {
-  componentDidMount() {
+  constructor() {
+  }
 
+  login(e) {
+    // pass in form data to server
+    e.preventDefault();
+    console.log('log me in');
   }
 
   render() {
     return(
       <div className="nav-container">
         <span className="nav-logo">T</span>
+
+        <form
+          className="login"
+          onSubmit={this.login}
+          >
+          <label>
+            <input
+              type="text"
+              placeholder="Username"
+              value={this.state.username}
+              />
+          </label>
+          <lable>
+            <input type="text" placeholder="Password"/>
+          </lable>
+          <button type="submit">Login</button>
+        </form>
+
       </div>
     )
   }
