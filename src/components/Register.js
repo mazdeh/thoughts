36d97e1cd983f08a4a8a63ccce1dcac729b225a3
Vahid Mazde <vahid.mazdeh@fourwindsinteractive.com>
2016-09-26 12:57:26 -0600

    form submission with React

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.register = this.register.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePassChange = this.handlePassChange.bind(this);
+
+    this.state = {
+      email: '',
+      username: '',
+      password: ''
+    }
+  }
+
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value })
+  }
+
+  handleUsernameChange(e) {
+    this.setState({ username: e.target.value })
+  }
+
+  handlePassChange(e) {
+    this.setState({ password: e.target.value })
+  }
+
   register(e) {
     e.preventDefault();
     console.log('form inputs?')
-    console.log('e: ', e);
+    console.log('e: ', this.state);
   }
 
   render() {
@@ -15,15 +41,27 @@ export default class Register extends Component {
           >
           <label>
             Email:
-            <input type="text" placeholder="email" />
+            <input
+              type="text"
+              placeholder="email"
+              value={this.state.email}
+              onChange={this.handleEmailChange} />
           </label>
           <label>
             Username:
-            <input type="text" placeholder="username" />
+            <input
+              type="text"
+              placeholder="username"
+              value={this.state.username}
+              onChange={this.handleUsernameChange} />
           </label>
           <label>
             Password:
-            <input type="text" placeholder="password" />
+            <input
+              type="text"
+              placeholder="password"
+              value={this.state.password}
+              onChange={this.handlePassChange} />
           </label>
 
           <button type="submit">Sign Up!</button>
