e9aac16997b198cf1c65b5ca48f4945bf7743f31
Vahid Mazde <vahid.mazdeh@fourwindsinteractive.com>
2016-12-04 18:58:53 -0700

    revert hot reloading! not necessary at thsi point. too much overhead for too little benifit

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
+// uncomment for hot-reloading!
 var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
   devtool: 'source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
+  entry: './src/index',
+  // entry: [
+  //   'webpack-hot-middleware/client',
+  //   'webpack/hot/only-dev-server',
+  //   './src/index'
+  // ],
   output: {
-    publicPath: '/',
-    path: path.resolve(__dirname, '/dist'),
+    path: './dist',
+    // publicPath: '/',
+    // path: path.resolve(__dirname, '/dist'),
     filename: 'bundle.js'
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      __DEV__: true
-    }),
-    new webpack.NoErrorsPlugin(), // no bundle if errored
-    new webpack.optimize.DedupePlugin(), // dependency
-    new webpack.optimize.OccurenceOrderPlugin(), // bundle order
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  // plugins: [
+  //   new webpack.DefinePlugin({
+  //     __DEV__: true
+  //   }),
+  //   new webpack.NoErrorsPlugin(), // no bundle if errored
+  //   new webpack.optimize.DedupePlugin(), // dependency
+  //   new webpack.optimize.OccurenceOrderPlugin(), // bundle order
+  //   new webpack.HotModuleReplacementPlugin()
+  // ],
   module: {
     loaders: [
       { test: /\.scss/, loader: 'style!css!sass' },
